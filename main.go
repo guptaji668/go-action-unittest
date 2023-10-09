0c6d13e6325e589b48fd20c705cf117267e93b26
Rohan Gupta <122445469+guptaji668@users.noreply.github.com>
2023-10-09 12:07:10 +0530

    test

--- main.go
+++ main.go
@@ -9,22 +9,8 @@ func Add(a, b int) int {
 }
 
 func main() {
-    var num1, num2 int
+    
 
-    fmt.Print("Enter the first number: ")
-    _, err1 := fmt.Scan(&num1)
-    if err1 != nil {
-        fmt.Println("Error reading the first number:", err1)
-        return
-    }
-
-    fmt.Print("Enter the second number: ")
-    _, err2 := fmt.Scan(&num2)
-    if err2 != nil {
-        fmt.Println("Error reading the second number:", err2)
-        return
-    }
-
-    result := Add(num1, num2)
+    result := Add(3,2)
     fmt.Printf("Result: %d\n", result)
 }
